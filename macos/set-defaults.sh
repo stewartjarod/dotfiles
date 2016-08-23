6658337736b4b3cb04c2662a7268ef61dd1ed62e
Jarod Stewart <jarod@simpleenergy.com>
2016-08-23 16:28:27 -0600

    macos go to sleep on bottom right hotspot

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -26,9 +26,9 @@ defaults write NSGlobalDomain KeyRepeat -int 0
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
-# Run the screensaver if we're in the bottom-left hot corner.
-defaults write com.apple.dock wvous-bl-corner -int 5
-defaults write com.apple.dock wvous-bl-modifier -int 0
+# Run the screensaver if we're in the bottom-right hot corner.
+defaults write com.apple.dock wvous-br-corner -int 5
+defaults write com.apple.dock wvous-br-modifier -int 0
 
 # Hide Safari's bookmark bar.
 defaults write com.apple.Safari ShowFavoritesBar -bool false
