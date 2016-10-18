97f13aadf44a69bbe634526112a732076630fae6
stewartjarod <jarod@simpleenergy.com>
2016-10-18 15:27:51 -0600

    Add installation of nvm. Fix yarn path.zsh file. Fix path issues

--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,14 @@
+#!/bin/sh
 if test ! $(which spoof)
 then
+  echo "› installing spoof"
   sudo npm install spoof -g
 fi
+
+if type nvm &> /dev/null
+then
+  echo "› installing nvm"
+  wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+fi
+
+exit 0
