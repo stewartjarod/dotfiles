1ef74c3590614868467ea48caffa497f5f9669b8
stewartjarod <jarod@simpleenergy.com>
2016-10-20 14:53:14 -0600

    add gup git pull upstream master

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -11,6 +11,8 @@ alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 
+alias gup='git pull upstream master'
+
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
