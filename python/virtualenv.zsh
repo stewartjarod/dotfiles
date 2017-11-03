47ea7a5c12c6ee48098244ba5d406d0167c5f846
Jarod Stewart <stewartjarod@gmail.com>
2017-11-02 21:20:03 -0600

    update python3 virtual env creation

--- python/virtualenv.zsh
+++ python/virtualenv.zsh
@@ -1,10 +1,14 @@
 
 function activate_virtual_env() {
   folder=$(basename $PWD)
-  virtual_path="$HOME/VirtualEnvs/$folder/bin"
+  virtual_path="$HOME/.virtualEnvs/$folder/bin"
   if [ -e "$virtual_path" ]
   then
     source "$virtual_path/activate"
   fi
 
+}
+
+function create_virtual_env() {
+  python3 -m venv $HOME/.virtualEnvs/$1
 }
