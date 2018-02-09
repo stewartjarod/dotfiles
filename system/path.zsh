47e0b3f282aa2c3e092cdeb5bfc887840cc1f00c
Jarod Stewart <jarod@yonomi.co>
2018-02-09 15:39:22 -0700

    Clean up, fix npm config prefix issue with pathing

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,3 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME.cargo/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export NVM_DIR="$HOME/.nvm"
-. "/usr/local/opt/nvm/nvm.sh"
