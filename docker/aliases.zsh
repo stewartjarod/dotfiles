4b821eb07c97628e30c54a55548e074b387e8afc
Jarod Stewart <jarod@yonomi.co>
2018-02-09 15:20:45 -0700

    add docker-nuke to delete all the things

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,3 +1,5 @@
 alias d='docker $*'
 alias d-c='docker-compose $*'
 alias dps='docker ps -a --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Names}}"'
+
+alias docker-nuke='docker rm -f $(docker ps -a -q) & docker rmi -f $(docker images -a -q) & docker volume rm $(docker volume ls -q) & docker network rm $(docker network ls | tail -n+2 | awk "{if($2 !~ /bridge|none|host/){ print $1 }}")'
