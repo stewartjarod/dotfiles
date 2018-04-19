f1249e142b2f2be895992316b948c0a1fe7d4987
Jarod Stewart <stewartjarod@gmail.com>
2018-04-19 17:59:21 -0600

    update initialKeyRepeat value

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME.cargo/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME.cargo/bin:/usr/local/Cellar/postgresql/10.3/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export NVM_DIR="$HOME/.nvm"
