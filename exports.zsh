8ea8c5398316082d0021c985b4ef4508e9ed4596
Jarod Stewart <jarod@yonomi.co>
2021-03-31 09:00:09 -0600

    oopsies

--- exports.zsh
+++
@@ -1,7 +0,0 @@
-export NPM_TOKEN=f9de2c6e-6896-4f2f-b19d-0528f19fae8a
-export AWS_KMS_KEYID=e9127211-9ac7-465d-9af7-602557fa1454
-export AWS_REGION=us-east-1
-export IS_LOCAL=true
-export LOG_DISABLED=true
-
-export UAT_JAROD=e63b1daa-9f4e-4e17-aaa7-6bae77769e27
